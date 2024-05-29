6253c731fa62866be58c975215c646a3997048e3
zen-lenovo <zennomi.kun@gmail.com>
2024-05-29 16:02:42 +0700

    update subsquid graphql endpoint

--- src/configs/apollo.tsx
+++ src/configs/apollo.tsx
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:4350/graphql',
+    uri: 'https://nftverse-subsquid.zenno.moe/graphql',
     cache: new InMemoryCache(),
 });
