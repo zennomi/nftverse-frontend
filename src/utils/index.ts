644b61385c6ab56180d2ed88dd38b9da2195c0b5
zen-lenovo <zennomi.kun@gmail.com>
2024-05-30 08:20:52 +0700

    fix wsrv.nl not whitelist our domain

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,6 +9,10 @@ export function getWeservUrl(url: string | undefined, config?: {
     maxage?: `${number}d` | `${number}w` | `${number}M` | `${number}y`
 }) {
     if (!url) url = "wsrv.nl/placeholder.svg"
+    if (url.startsWith("https://nftverse-backend.zenno.moe")) return url;
+    url = getIPFSUri(url)
+    if (url.startsWith("https://")) url = url.replace("https://", "//");
+    if (url.startsWith("http://")) url = url.replace("http://", "//");
     return `https://wsrv.nl/?url=${encodeURI(getIPFSUri(url))}${config?.w ? '&w=' + config.w : ''}${config?.h ? '&h=' + config.h : ''}${config?.maxage ? '&maxage=' + config.maxage : ''}`
 }
 
